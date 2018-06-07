17681b0ec898eb20b77373b7284f877493de3295
Chad Cuddigan <cuddigan@gmail.com>
2018-06-06 20:18:33 -0700

    Adding a shell script that can build the SDL dependencies needed

---
+++ build_deps.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+SDL_version=2.0.8
+SDL_mixer_version=2.0.2
+
+if [ -d ./built_ext/ ]; then
+	echo A directory named build_ext already exists.
+	echo Please remove it if you want to recompile.
+	exit
+fi
+
+mkdir ./build_ext/
+cd ./build_ext/
+
+install_dir=$(pwd)
+
+function build_sdl {
+	curl -O https://www.libsdl.org/release/SDL2-${SDL_version}.tar.gz
+	tar xvf SDL2-${SDL_version}.tar.gz
+	pushd SDL2-${SDL_version}
+
+	./configure
+	make
+	make install
+
+	popd
+}
+
+function build_sdl_mixer {
+	curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-${SDL_mixer_version}.tar.gz
+	tar xvf SDL2_mixer-${SDL_mixer_version}.tar.gz
+	pushd SDL2_mixer-${SDL_mixer_version}
+
+	./configure
+	make
+	make install
+
+	popd
+}
+
+build_sdl
+build_sdl_mixer
