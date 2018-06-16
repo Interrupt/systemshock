043cb96bdfe0364ec254a81724e6a98baa6f3646
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2018-06-16 19:18:02 +0300

    Simplify long_sqrt()

--- src/Libraries/FIX/Source/fix_sqrt.c
+++ src/Libraries/FIX/Source/fix_sqrt.c
@@ -32,26 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <math.h>
 #include <stdio.h>
 
-//--------------------
-//  Table of square root guesses
-//--------------------
-ubyte pGuessTable[256] = {
-    1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  4,
-    4,  4,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  6,  6,
-    6,  6,  6,  6,  6,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
-    8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  9,  9,  9,
-    9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  10, 10, 10, 10,
-    10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11,
-    11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-    12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
-    12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14,
-    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
-    14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
-    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
-    15, 15, 15, 15, 15, 15, 15, 15, 15};
-
-
 //-----------------------------------------------------------------
 //  Calculate the square root of a fixed-point number.
 //-----------------------------------------------------------------
@@ -76,7 +56,7 @@ int32_t quad_sqrt(int32_t hi, uint32_t lo) {
 
   // If a negative number, return 0.
   if (hi < 0) {
-    printf("quad_sqrt of negative number!");
+    printf("quad_sqrt of negative number!\n");
     return (0);
   }
 
@@ -87,81 +67,14 @@ int32_t quad_sqrt(int32_t hi, uint32_t lo) {
 //  Calculate the square root of a long number.
 //-----------------------------------------------------------------
 int long_sqrt(int num) {
-  fix savediv;
-  fix temp;
-  uchar testb, trans;
-  uchar shift;
-  fix divisor;
-  short i;
-
-  if (num == 0) // A bit of error checking.
+  // WH dunno, needed?
+  if (num == 0)
     return (0);
-  if (num < 0)
+  // A bit of error checking.
+  if (num < 0) {
+    printf("long_sqrt of negative number!\n");
     return (0);
-
-  // Find the highest byte that is non-zero, look up a good first guess for
-  // that byte, and shift it the appropriate amount.
-
-  testb = (uchar)(num >> 24);
-  if (testb != 0) {
-    trans = pGuessTable[testb];
-    shift = 12;
-    goto found_byte;
-  }
-  testb = (uchar)(num >> 16);
-  if (testb != 0) {
-    trans = pGuessTable[testb];
-    shift = 8;
-    goto found_byte;
-  }
-  testb = (uchar)(num >> 8);
-  if (testb != 0) {
-    trans = pGuessTable[testb];
-    shift = 4;
-    goto found_byte;
-  }
-  testb = (uchar)num;
-  if (testb != 0) {
-    trans = pGuessTable[testb];
-    shift = 0;
   }
 
-  // We now have the good initial guess.  Shift it the appropriate amount.
-
-found_byte:
-  divisor = trans;
-  divisor = divisor << shift;
-
-  // Experience has shown that we almost always go through the loop
-  // just about three times.  To avoid compares and jumps, we iterate
-  // three times without even thinking about it, and then start checking
-  // to see if our answer is correct.
-
-  for (i = 0; i < 2; i++) {
-    temp = num / divisor;
-    divisor += temp;
-    divisor = divisor >> 1;
-  }
-
-  // Starting with the third iteration, we now actually check for a match.
-
-  while (1) {
-    temp = num / divisor;
-    if (temp == divisor)
-      break;
-
-    savediv = divisor;
-    divisor += temp;
-    divisor = divisor >> 1;
-    if (temp == divisor) {
-      if (num % savediv != 0)
-        divisor++;
-      break;
-    } else if (savediv == divisor) {
-      if (num % savediv != 0)
-        divisor++;
-      break;
-    }
-  }
-  return (divisor);
+  return (int32_t)sqrt(num);
 }
