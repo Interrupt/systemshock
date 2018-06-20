f7ece59ef22382bdf39c74374893f437a219a380
mrikola <markku.rikola@gmail.com>
2018-06-20 22:47:48 +0300

    more instructions to build_msvc.sh

--- build_msvc.sh
+++ build_msvc.sh
@@ -24,11 +24,15 @@ tar xvf SDL2-${SDL_version}.tar.gz
 git clone https://github.com/SDL-mirror/SDL_mixer.git
 
 cd ..
-
+'
 echo "Next steps: "
-echo "1. Build SDL and SDL_mixer in Visual Studio"
-echo "2. Find the DLL's created this way"
-echo "3. Run CMake-GUI to create Visual Studio project for Shockolate"
-echo "4. Add $(ProjectDir)src\GameSrc\Headers\precompiled.h to systemshock and GAME_LIB"
+echo "1. Build SDL2, SDL2main and SDL_mixer in Visual Studio"
+echo "   (you'll need to add SDL2's include and output directories "
+echo "   and linker dependencies respectively) "
+echo "2. Copy the DLL's in the output directories to this directory"
+echo "3. Run CMake-GUI to create a Visual Studio project for Shockolate"
+echo "4. Add \$(ProjectDir)\src\GameSrc\Headers\precompiled.h to systemshock and GAME_LIB"
 echo "   in Properties -> Configuration Properties -> C/C++ "
 echo "   -> Advanced -> Forced Include File "
+echo "5. If the linker complains about 'm.lib', remove it from systemshock's dependencies"
+echo "   in Properties -> Configuration Properties -> C/C++ -> Linker -> Input"
