0f6ddbce0263b102e42733e497a0dce3dd92ff31
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2018-06-27 00:54:48 +0300

    Rewrote vector.c, last AWide* functions user.

--- src/Libraries/3D/Source/vector.c
+++ src/Libraries/3D/Source/vector.c
@@ -46,17 +46,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "fix.h"
 #include "lg.h"
 
-// double a 64 bit long (two 32's), basically a shift left
-#define Double_64(a, b) \
-  if (((long)b) < 0) {  \
-    b <<= 1;            \
-    a <<= 1;            \
-    a++;                \
-  } else {              \
-    b <<= 1;            \
-    a <<= 1;            \
-  }
-
 // prototypes
 void g3_compute_normal_quick(g3s_vector *v, g3s_vector *v0, g3s_vector *v1, g3s_vector *v2);
 
@@ -123,86 +112,80 @@ void g3_vec_normalize(g3s_vector *v) {
 // trashes eax,ebx,ecx,edx,esi
 // the quick version does not normalize
 void g3_compute_normal_quick(g3s_vector *v, g3s_vector *v0, g3s_vector *v1, g3s_vector *v2) {
-  AWide result, result2;
+  int64_t r;
   g3s_vector temp_v0;
   g3s_vector temp_v1;
   g3s_vector temp_high;
-  long temp_long;
-  int shiftcount;
+  int32_t temp_long;
+  int32_t shiftcount;
 
   g3_vec_sub(&temp_v0, v1, v0);
   g3_vec_sub(&temp_v1, v2, v1);
 
   // dest->x = v1z * v0y - v1y * v0z;
-  AsmWideMultiply(temp_v1.gZ, temp_v0.gY, &result);
-  AsmWideMultiply(temp_v1.gY, temp_v0.gZ, &result2);
-  AsmWideNegate(&result2);
-  AsmWideAdd(&result, &result2);
-  v->gX = result.lo;
-  temp_high.gX = result.hi;
+  r = fix64_mul(temp_v1.gZ, temp_v0.gY) - fix64_mul(temp_v1.gY, temp_v0.gZ);
+  v->gX = fix64_frac(r);
+  temp_high.gX = fix64_int(r);
 
   // dest->y = v1x * v0z - v1z * v0x;
-  AsmWideMultiply(temp_v1.gX, temp_v0.gZ, &result);
-  AsmWideMultiply(temp_v1.gZ, temp_v0.gX, &result2);
-  AsmWideNegate(&result2);
-  AsmWideAdd(&result, &result2);
-  v->gY = result.lo;
-  temp_high.gY = result.hi;
+  r = fix64_mul(temp_v1.gX, temp_v0.gZ) - fix64_mul(temp_v1.gZ, temp_v0.gX);
+  v->gY = fix64_frac(r);
+  temp_high.gY = fix64_int(r);
 
   // dest->z = v1y * v0x - v1x * v0y;
-  AsmWideMultiply(temp_v1.gY, temp_v0.gX, &result);
-  AsmWideMultiply(temp_v1.gX, temp_v0.gY, &result2);
-  AsmWideNegate(&result2);
-  AsmWideAdd(&result, &result2);
-  v->gZ = result.lo;
-  temp_high.gZ = result.hi;
+  r = fix64_mul(temp_v1.gY, temp_v0.gX) - fix64_mul(temp_v1.gX, temp_v0.gY);
+  v->gZ = fix64_frac(r);
+  temp_high.gZ = fix64_int(r);
 
   // see if fit into a longword
-  result.hi = temp_high.gX;
-  result.lo = v->gX;
-  if (result.hi < 0)
-    AsmWideNegate(&result);
-  Double_64(result.hi, result.lo);
-  temp_long = result.hi;
+  r = fix64_make(temp_high.gX, v->gX);
+  if (temp_high.gX < 0)
+    r = -r;
+  temp_long = fix64_int(2 * r);
 
-  result.hi = temp_high.gY;
-  result.lo = v->gY;
-  if (result.hi < 0)
-    AsmWideNegate(&result);
-  Double_64(result.hi, result.lo);
-  temp_long |= result.hi;
+  r = fix64_make(temp_high.gY, v->gY);
+  if (temp_high.gY < 0)
+    r = -r;
+  temp_long |= fix64_int(2 * r);
 
-  result.hi = temp_high.gZ;
-  result.lo = v->gZ;
-  if (result.hi < 0)
-    AsmWideNegate(&result);
-  Double_64(result.hi, result.lo);
-  temp_long |= result.hi;
+  r = fix64_make(temp_high.gZ, v->gZ);
+  if (temp_high.gZ < 0)
+    r = -r;
+  temp_long |= fix64_int(2 * r);
 
   if (!temp_long)
     return; // everything fits in the low longword. hurrah. see ya.
 
   // see how far to shift to fit in a longword
   shiftcount = 0;
-  while (((unsigned long)temp_long) >= 0x0100) {
+  while (((uint32_t)temp_long) >= 0x0100) {
     shiftcount += 8;
     temp_long >>= 8;
   }
   shiftcount += shift_table[temp_long];
 
   // now get the results
-  result.hi = temp_high.gX;
-  result.lo = v->gX;
-  AsmWideBitShift(&result, shiftcount);
-  v->gX = result.lo;
+  r = fix64_make(temp_high.gX, v->gX);
+  if (shiftcount < 0) {
+    r <<= -shiftcount;
+  } else {
+    r >>= shiftcount;
+  }
+  v->gX = fix64_frac(r);
 
-  result.hi = temp_high.gY;
-  result.lo = v->gY;
-  AsmWideBitShift(&result, shiftcount);
-  v->gY = result.lo;
+  r = fix64_make(temp_high.gY, v->gY);
+  if (shiftcount < 0) {
+    r <<= -shiftcount;
+  } else {
+    r >>= shiftcount;
+  }
+  v->gY = fix64_frac(r);
 
-  result.hi = temp_high.gZ;
-  result.lo = v->gZ;
-  AsmWideBitShift(&result, shiftcount);
-  v->gZ = result.lo;
+  r = fix64_make(temp_high.gZ, v->gZ);
+  if (shiftcount < 0) {
+    r <<= -shiftcount;
+  } else {
+    r >>= shiftcount;
+  }
+  v->gZ = fix64_frac(r);
 }
