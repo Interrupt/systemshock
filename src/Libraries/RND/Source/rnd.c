Interrupt/systemshock

commit 5ce973d09cb240e7063e8cca10497558375c4304

Using stdlib rand for random numbers for now

src/Libraries/RND/Source/rnd.c
@@ -152,6 +152,7 @@ ulong asm high_umpy(ulong a, ulong b)
 
  ulong high_umpy(ulong a, ulong b)
  {
+ 	printf("FIXME: high_umpy\n");
  	return a * b;
  }
 
@@ -169,7 +170,8 @@ ulong asm high_umpy(ulong a, ulong b)
 
 long RndRange(RndStream *prs, long low, long high)
 {
-	return(low + high_umpy(Rnd(prs), (high - low) + 1));
+	// HAX HAX HAX should use this library instead of using the std lib random
+	return (rand() % (high - low)) + low;
 }
 
 //	----------------------------------------------------------------
@@ -184,7 +186,10 @@ long RndRange(RndStream *prs, long low, long high)
 
 fix RndRangeFix(RndStream *prs, fix low, fix high)
 {
-	return(low + (fix_mul(RndFix(prs), high - low)));
+	// HAX HAX HAX should use this library instead of using the std lib random
+	float flow = fix_float(low);
+	float fhigh = fix_float(high);
+	return fix_from_float((float)rand()/(float)(RAND_MAX/(fhigh - flow)) + flow);
 }
 
 //	-----------------------------------------------------------------

src/GameSrc/player.c
@@ -144,8 +144,8 @@ errtype init_player(Player *pplr)
    for (i=0; i < NUM_INIT_QV; i ++)
       QUESTVAR_SET(init_questvars[i][0], init_questvars[i][1]);
 
-   printf("Skipping randomizing reactor combo, loops forever!\n");
-   /*while(QUESTVAR_GET(REACTOR_COMBO_QVAR)==QUESTVAR_GET(REACTOR_COMBO_QVAR+1)) {
+   //printf("Skipping randomizing reactor combo, loops forever!\n");
+   while(QUESTVAR_GET(REACTOR_COMBO_QVAR)==QUESTVAR_GET(REACTOR_COMBO_QVAR+1)) {
       // randomize reactor combination. use effect_rnd 'cause why not.
       j = (RndRange(&effect_rnd,0,9)<<8)|(RndRange(&effect_rnd,0,9)<<4)
          | RndRange(&effect_rnd,0,9);
@@ -153,7 +153,7 @@ errtype init_player(Player *pplr)
       j = (RndRange(&effect_rnd,0,9)<<8)|(RndRange(&effect_rnd,0,9)<<4)
          | RndRange(&effect_rnd,0,9);
       QUESTVAR_SET(REACTOR_COMBO_QVAR+1,j);
-   }*/
+   }
    QUESTVAR_SET(LANGUAGE_QVAR,which_lang);
 
    pplr->fatigue_regen = 0;

commit 24e64119eec00d804190615adebab0534653ead6

Change RndRange() function to include both limits
The RndRange() function should return a random integer number between
low and high inclusive. When low and high are equal, it must return
that value (instead of crashing with an arithmetic exception). This
commit fixes a regression from 5ce973d0.

Fixes #93

src/Libraries/RND/Source/rnd.c
@@ -170,7 +170,7 @@ ulong asm high_umpy(ulong a, ulong b)
 long RndRange(RndStream *prs, long low, long high)
 {
 	// HAX HAX HAX should use this library instead of using the std lib random
-	return (rand() % (high - low)) + low;
+	return (rand() % (high - low + 1)) + low;
 }
 
 //	----------------------------------------------------------------