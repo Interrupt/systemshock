022f408c724a1481d2b341a719e9bdc21de4e3aa
Chad Cuddigan <cuddigan@gmail.com>
2018-07-18 23:20:06 -0700

    Removing some double includes for SDL OpenGL

--- src/MacSrc/OpenGL.cc
+++ src/MacSrc/OpenGL.cc
@@ -9,8 +9,6 @@
 #else
 #include <GL/gl.h>
 #include <GL/glext.h>
-#include <SDL.h>
-#include <SDL_opengl.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
