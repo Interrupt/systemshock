e474924c58d6614031c8f7dadeb9117efd6d6cbe
mrikola <markku.rikola@gmail.com>
2018-07-21 03:54:16 +0300

    fix tabs like the pedantic monster I am

--- src/MacSrc/Shock.c
+++ src/MacSrc/Shock.c
@@ -173,14 +173,14 @@ int main(int argc, char** argv)
 	mainloop(argc, argv);
 
 	status_bio_end();
-    stop_music();
+	stop_music();
 
-    /*
+	/*
 	// We're through playing now.
 	uiHideMouse(NULL);
 	loopmode_exit(_current_loop);
 	status_bio_end();
-     stop_music();											//KLC - add here to stop music at end game
+	 stop_music();											//KLC - add here to stop music at end game
 	
 	if (gDeadPlayerQuit)									// If we quit because the player was killed, show
 	{																// the death movie.
@@ -238,15 +238,15 @@ void InitSDL()
 	}
 
 	// TODO: figure out some universal set of settings that work...
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
 
@@ -264,11 +264,11 @@ void InitSDL()
 
 	gr_init();
 
-    gr_set_mode(GRM_320x200x8, TRUE);
+	gr_set_mode(GRM_320x200x8, TRUE);
 
-    INFO("Setting up screen and render contexts");
+	INFO("Setting up screen and render contexts");
 
-    // Open our window!
+	// Open our window!
 
 	window = SDL_CreateWindow(
 		"System Shock - Shockolate 0.5", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -281,8 +281,8 @@ void InitSDL()
 
 	// Setup the screen
 
-    svga_screen = cit_screen = gr_alloc_screen(grd_cap->w, grd_cap->h);
-    gr_set_screen(svga_screen);
+	svga_screen = cit_screen = gr_alloc_screen(grd_cap->w, grd_cap->h);
+	gr_set_screen(svga_screen);
 
 	gr_alloc_ipal();
 
