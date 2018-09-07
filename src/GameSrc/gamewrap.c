f4c23554b1cfe29d88036aa80f99f42215b144ee
donnierussellii <donnie.russell.ii@gmail.com>
2018-09-06 22:28:35 -0400

    Fix title screen continue option

--- src/GameSrc/gamewrap.c
+++ src/GameSrc/gamewrap.c
@@ -97,6 +97,8 @@ errtype copy_file(char *src_fname, char *dest_fname) {
     FILE *fsrc, *fdst;
     DEBUG("copy_file: %s to %s", src_fname, dest_fname);
 
+	if (!strcmp(src_fname, dest_fname)) return OK; //copying file to itself
+
     fsrc = fopen_caseless(src_fname, "rb");
     if (fsrc == NULL) {
         return ERR_FOPEN;
