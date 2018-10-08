6f95332788fff252e435c7275832a70c1dee57d3
Manuel K <Manuel-K@users.noreply.github.com>
2018-10-08 23:48:37 +0200

    Use SDL2_mixer 2.0.2 with backport
    Apply patch to fix voc magic

--- osx-linux/install_32bit_sdl.sh
+++ osx-linux/install_32bit_sdl.sh
@@ -16,10 +16,14 @@ function build_sdl {
 	popd
 }
 
+SDL2_mixer_version=2.0.2
+
 function build_sdl_mixer {
-	git clone --depth 10 https://github.com/SDL-mirror/SDL_mixer.git
-	pushd SDL_mixer
-	git checkout -b 23-05-2018 7cad09d
+	curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-${SDL2_mixer_version}.tar.gz
+	tar xf SDL2_mixer-${SDL2_mixer_version}.tar.gz
+	pushd SDL2_mixer-${SDL2_mixer_version}
+	curl -O https://github.com/SDL-mirror/SDL_mixer/commit/7cad09d4d479df2b21b3e489f8e155bdf8254fd4.patch
+	patch < 7cad09d4d479df2b21b3e489f8e155bdf8254fd4.patch
 
 	export SDL2_CONFIG="/usr/local/bin/sdl2-config"
 	./configure "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
