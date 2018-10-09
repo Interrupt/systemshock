7ad6f37e88ae416edd231133b90e73bdd2bd9fb8
Manuel K <Manuel-K@users.noreply.github.com>
2018-10-09 22:52:11 +0200

    build_deps.sh: Use SDL2_mixer 2.0.2 with backport
    Apply patch to fix voc magic

--- build_deps.sh
+++ build_deps.sh
@@ -2,7 +2,7 @@
 set -e
 
 SDL_version=2.0.8
-SDL_mixer_version=2.0.2
+SDL2_mixer_version=2.0.2
 
 if [ -d ./build_ext/ ]; then
 	echo A directory named build_ext already exists.
@@ -28,10 +28,11 @@ function build_sdl {
 }
 
 function build_sdl_mixer {
-	git clone https://github.com/SDL-mirror/SDL_mixer.git
-	#tar xvf SDL2_mixer-${SDL_mixer_version}.tar.gz
-	#pushd SDL2_mixer-${SDL_mixer_version}
-	pushd SDL_mixer
+	curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-${SDL2_mixer_version}.tar.gz
+	tar xvf SDL2_mixer-${SDL2_mixer_version}.tar.gz
+	pushd SDL2_mixer-${SDL2_mixer_version}
+	curl -O https://github.com/SDL-mirror/SDL_mixer/commit/7cad09d4d479df2b21b3e489f8e155bdf8254fd4.patch
+	patch < 7cad09d4d479df2b21b3e489f8e155bdf8254fd4.patch
 
   export SDL2_CONFIG="${install_dir}/built_sdl/bin/sdl2-config"
 	./configure "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32" --prefix=${install_dir}/built_sdl_mixer
