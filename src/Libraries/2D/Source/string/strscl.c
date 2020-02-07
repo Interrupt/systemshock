d580053ba079af7c8e035184295cf957e6f3c1e5
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2020-02-08 01:09:32 +0300

    clang-tidy on 2d/string

--- src/Libraries/2D/Source/string/strscl.c
+++ src/Libraries/2D/Source/string/strscl.c
@@ -6,15 +6,15 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+
 */
 /* $Source: n:/project/lib/src/2d/RCS/strscl.c $
  * $Revision: 1.2 $
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* scale each character as a separate bitmap: use std scaling calculation
-   to set the ul of each (next) pixmap and set w/h for bitmap scaling 
+   to set the ul of each (next) pixmap and set w/h for bitmap scaling
    routine
 
    this is hugely inefficient, like the old unscaled string routines
@@ -40,70 +40,64 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdio.h> // printf()
 
-int gen_font_scale_string (grs_font *f, char *s, short x0, short y0, short w, short h)
-{
-   grs_bitmap bm;             /* character bitmap */
-   short *offset_tab;         /* table of character offsets */
-   uchar *char_buf;           /* font pixel data */
-   short offset;              /* offset of current character */
-   short str_w, str_h;        /* width and height of src string */
-   fix x, y;                  /* position of current character */
-   fix x_scale, y_scale;      /* x and y scale factors */
-   fix next_x, next_y, del_y; /* need to use next, del_y since it's const */
-   int i;                     
-   uchar c;                    /* current character */
+int gen_font_scale_string(grs_font *f, char *s, short x0, short y0, short w, short h) {
+    grs_bitmap bm;             /* character bitmap */
+    short *offset_tab;         /* table of character offsets */
+    uchar *char_buf;           /* font pixel data */
+    short offset;              /* offset of current character */
+    short str_w, str_h;        /* width and height of src string */
+    fix x, y;                  /* position of current character */
+    fix x_scale, y_scale;      /* x and y scale factors */
+    fix next_x, next_y, del_y; /* need to use next, del_y since it's const */
+    int i;
+    uchar c; /* current character */
 
-   if(w==0 || h==0) {
-      return CLIP_NONE;
-   }
+    if (w == 0 || h == 0) {
+        return CLIP_NONE;
+    }
 
-   char_buf = (uchar *)f + f->buf;
-   offset_tab = f->off_tab;
-   gr_init_bm (&bm, NULL, (f->id==0xcccc)? BMT_FLAT8: BMT_MONO,
-               BMF_TRANS, 0, f->h);
-   bm.row = f->w;
+    char_buf = (uchar *)f + f->buf;
+    offset_tab = f->off_tab;
+    gr_init_bm(&bm, NULL, (f->id == 0xcccc) ? BMT_FLAT8 : BMT_MONO, BMF_TRANS, 0, f->h);
+    bm.row = f->w;
 
-   gr_font_string_size (f, s, &str_w, &str_h);
+    gr_font_string_size(f, s, &str_w, &str_h);
 
-   x_scale = (w << 16) / str_w;
-   y_scale = (h << 16) / str_h;
+    x_scale = (w << 16) / str_w;
+    y_scale = (h << 16) / str_h;
 
-   x = x0<<16; y = y0<<16;
+    x = x0 << 16;
+    y = y0 << 16;
 
-   for (i=0, del_y = 0; i < f->h; del_y += y_scale, i++);  /* multiply fix by int, faster ?? */
-   next_y = y + del_y; 
-  
-   while ((c = (uchar)(*s++)) != '\0') {
-      if (c=='\n' || c==CHAR_SOFTCR) {
-         x = x0<<16;
-         y = next_y;
-	 next_y = y + del_y;
-         continue;
-      }
-      if (c>f->max || c<f->min || c==CHAR_SOFTSP)
-         continue;
-      offset = offset_tab[c-f->min];
-      bm.w = offset_tab[c-f->min+1]-offset;
+    for (i = 0, del_y = 0; i < f->h; del_y += y_scale, i++)
+        ; /* multiply fix by int, faster ?? */
+    next_y = y + del_y;
 
-      for (i=0, next_x = x; i < bm.w; next_x += x_scale, i++);  /* multiply fix by int, faster ?? */
+    while ((c = (uchar)(*s++)) != '\0') {
+        if (c == '\n' || c == CHAR_SOFTCR) {
+            x = x0 << 16;
+            y = next_y;
+            next_y = y + del_y;
+            continue;
+        }
+        if (c > f->max || c < f->min || c == CHAR_SOFTSP)
+            continue;
+        offset = offset_tab[c - f->min];
+        bm.w = offset_tab[c - f->min + 1] - offset;
 
-      if (bm.type == BMT_MONO) {
-         bm.bits = char_buf + (offset>>3);
-         bm.align = offset&7;
-         gr_scale_bitmap (&bm, fix_int (x), fix_int (y), 
-				fix_int (next_x) - fix_int(x), 
-				fix_int (next_y) - fix_int (y));
+        for (i = 0, next_x = x; i < bm.w; next_x += x_scale, i++)
+            ; /* multiply fix by int, faster ?? */
 
-       }
-      else {
-         bm.bits = char_buf + offset;
-         gr_scale_bitmap (&bm, fix_int (x), fix_int (y), 
-			  fix_int (next_x) - fix_int(x), 
-			  fix_int (next_y) - fix_int (y));
-      }
-      x = next_x;
-   }
-   return CLIP_NONE;
+        if (bm.type == BMT_MONO) {
+            bm.bits = char_buf + (offset >> 3);
+            bm.align = offset & 7;
+            gr_scale_bitmap(&bm, fix_int(x), fix_int(y), fix_int(next_x) - fix_int(x), fix_int(next_y) - fix_int(y));
+
+        } else {
+            bm.bits = char_buf + offset;
+            gr_scale_bitmap(&bm, fix_int(x), fix_int(y), fix_int(next_x) - fix_int(x), fix_int(next_y) - fix_int(y));
+        }
+        x = next_x;
+    }
+    return CLIP_NONE;
 }
-
-
--- src/Libraries/2D/Source/string/strwrap.c
+++ src/Libraries/2D/Source/string/strwrap.c
@@ -6,17 +6,16 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
-*/
 
+*/
 
 /* $Source: r:/prj/lib/src/2d/RCS/strwrap.c $
  * $Revision: 1.3 $
@@ -24,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * $Date: 1994/08/19 02:39:22 $
  */
 
-/* wrapping and unwrapping routines moved into the 2d library, other 
+/* wrapping and unwrapping routines moved into the 2d library, other
    routines already present in 2d
 */
 
@@ -35,39 +34,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //		and for calculating the area needed to display
 //		text in a font, including automatic wrapping.
 
-
-/* log from /project/ff/code/gfx/font.c 
+/* log from /project/ff/code/gfx/font.c
 
  * Revision 1.6  1993/11/18  11:15:50  rex
  * Changed Font* to grs_font*
- * 
+ *
  * Revision 1.5  1993/11/04  21:37:40  kaboom
  * Changed quotes to angle brackets in includes for watcom
- * 
+ *
  * Revision 1.4  1993/05/04  13:47:45  rex
  * Fixed return value in FontWrapText()
- * 
+ *
  * Revision 1.3  1993/04/22  19:37:14  rex
  * Removed FontSetFont, added pfont param to other funcs
- * 
+ *
  * Revision 1.2  1993/02/04  12:24:09  rex
  * Converted to new debug system
- * 
+ *
  * Revision 1.1  1992/08/31  16:16:53  unknown
  * Initial revision
- * 
+ *
 */
 
 #include "lg_types.h"
 #include "chr.h"
 #include "ctxmac.h"
 
-static short *pCharPixOff;		// ptr to char offset table, with pfont->minch
-					// already subtracted out!
+static short *pCharPixOff; // ptr to char offset table, with pfont->minch
+                           // already subtracted out!
 
-#define CHARALIGN(pfont,c) (pCharPixOff[(uchar)c] & 7)
-#define CHARPTR(pfont,c) (&pfont->bits[pCharPixOff[(uchar)c] >> 3])
-#define CHARWIDTH(pfont,c) (pCharPixOff[(uchar)c+1] - pCharPixOff[(uchar)c])
+#define CHARALIGN(pfont, c) (pCharPixOff[(uchar)c] & 7)
+#define CHARPTR(pfont, c) (&pfont->bits[pCharPixOff[(uchar)c] >> 3])
+#define CHARWIDTH(pfont, c) (pCharPixOff[(uchar)c + 1] - pCharPixOff[(uchar)c])
 
 #define FONT_SETFONT(pfont) (pCharPixOff = &(pfont)->off_tab[0] - (pfont)->min)
 
@@ -85,89 +83,81 @@ static short *pCharPixOff;		// ptr to char offset table, with pfont->minch
 
 /* renamed to gr_font_string_wrap in library */
 
-int gr_font_string_wrap (grs_font *pfont, char *ps, short width)
-{
-	uchar *p;
-	char *pmark;
-	short numLines;
-	short currWidth;
+int gr_font_string_wrap(grs_font *pfont, char *ps, short width) {
+    uchar *p;
+    char *pmark;
+    short numLines;
+    short currWidth;
 
-//	Set up to do wrapping
+    //	Set up to do wrapping
 
-	FONT_SETFONT(pfont);
-	numLines = 0;					// ps = base of current line
+    FONT_SETFONT(pfont);
+    numLines = 0; // ps = base of current line
 
-//	Do wrapping for each line till hit end
+    //	Do wrapping for each line till hit end
 
-	while (*ps)
-		{
-		pmark = NULL;				// no SOFTCR insert point yet
-		currWidth = 0;				// and zero width so far
-		p = (uchar *) ps;
+    while (*ps) {
+        pmark = NULL;  // no SOFTCR insert point yet
+        currWidth = 0; // and zero width so far
+        p = (uchar *)ps;
 
-//	Loop thru each word
+        //	Loop thru each word
 
-		while (*p)
-			{
+        while (*p) {
 
-//	Skip through to next CR or space or '\0', keeping track of width
+            //	Skip through to next CR or space or '\0', keeping track of width
 
-			while ((*p != 0) && (*p != '\n') && (*p != ' '))
-				{
-				currWidth += CHARWIDTH(pfont, *p);
-				p++;
-				}
+            while ((*p != 0) && (*p != '\n') && (*p != ' ')) {
+                currWidth += CHARWIDTH(pfont, *p);
+                p++;
+            }
 
-//	If bypassed width, break out of word loop
+            //	If bypassed width, break out of word loop
 
-			if (currWidth > width)
-				{
-				if ((pmark == NULL) && (*p != 0) && (*p != '\n'))
-					pmark = (char *) p;
-				break;
-				}
+            if (currWidth > width) {
+                if ((pmark == NULL) && (*p != 0) && (*p != '\n'))
+                    pmark = (char *)p;
+                break;
+            }
 
-//	Else set new mark point (unless eol or eos, then bust out)
+            //	Else set new mark point (unless eol or eos, then bust out)
 
-			else
-				{
-				if ((*p == 0) || (*p == '\n'))	// hit end of line, wipe marker
-					{
-					pmark = NULL;
-					break;
-					}
-				pmark = (char *) p;									// else advance marker
-				currWidth += CHARWIDTH(pfont, ' ');	// and account for space
-				p++;
-				}
-			}
+            else {
+                if ((*p == 0) || (*p == '\n')) // hit end of line, wipe marker
+                {
+                    pmark = NULL;
+                    break;
+                }
+                pmark = (char *)p;                  // else advance marker
+                currWidth += CHARWIDTH(pfont, ' '); // and account for space
+                p++;
+            }
+        }
 
-//	Now insert soft cr if marked one
+        //	Now insert soft cr if marked one
 
-		if (pmark)
-			{
-			*pmark = CHAR_SOFTCR;
-			ps = pmark + 1;
-			if (*ps == ' ')			// if wrapped and following space,
-				*ps++ = CHAR_SOFTSP;	// turn into (ignored) soft space
-			}
+        if (pmark) {
+            *pmark = CHAR_SOFTCR;
+            ps = pmark + 1;
+            if (*ps == ' ')          // if wrapped and following space,
+                *ps++ = CHAR_SOFTSP; // turn into (ignored) soft space
+        }
 
-//	Otherwise, bump past cr
-		else
-			{
-			if (*p)
-				++p;
-			ps = (char *) p;
-			}
+        //	Otherwise, bump past cr
+        else {
+            if (*p)
+                ++p;
+            ps = (char *)p;
+        }
 
-//	Bump line counter in any case
+        //	Bump line counter in any case
 
-		++numLines;
-		}
+        ++numLines;
+    }
 
-//	When hit end of string, return # lines encountered
+    //	When hit end of string, return # lines encountered
 
-	return(numLines);
+    return (numLines);
 }
 
 //	--------------------------------------------------------
@@ -180,17 +170,12 @@ int gr_font_string_wrap (grs_font *pfont, char *ps, short width)
 
 /* renamed to gr_font_string_unwrap in 2d library */
 
-void gr_font_string_unwrap (char *s)
-{
-	int c;
+void gr_font_string_unwrap(char *s) {
+    int c;
 
-	while ((c = *s) != 0)
-		{
-		if ((c == CHAR_SOFTCR) || (c == CHAR_SOFTSP))
-			*s = ' ';
-		s++;
-		}
+    while ((c = *s) != 0) {
+        if ((c == CHAR_SOFTCR) || (c == CHAR_SOFTSP))
+            *s = ' ';
+        s++;
+    }
 }
-
-
-
