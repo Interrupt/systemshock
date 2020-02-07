2f613faa210e312ea278c62c62c096f64188f0c7
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2020-02-08 01:49:10 +0300

    Copyrights on 2d/string

--- src/Libraries/2D/Source/string/strscl.c
+++ src/Libraries/2D/Source/string/strscl.c
@@ -1,6 +1,7 @@
 /*
 
 Copyright (C) 2015-2018 Night Dive Studios, LLC.
+Copyright (C) 2018-2020 Shockolate Project
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
